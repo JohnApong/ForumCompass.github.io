fa2675401f2d7de26111235c2a93939b4177ea11
JohnApong <157560672+JohnApong@users.noreply.github.com>
2024-01-24 00:48:00 -0500

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,61 +1,59 @@
 questions = 
     [[//Left-Right`
         [//Right
-            "Freedom of business is the best practical way a society can prosper.",
-            "Charity is a better way of helping those in need than social welfare.",
-            "Wages are always fair, as employers know best what a worker's labour is worth.",
-            "It is \"human nature\" to be greedy.",
-            "\"Exploitation\" is an outdated term, as the struggles of 1800s capitalism doesn't exist anymore.",
-            "Communism is an ideal that can never work in practice.",
-            "Taxation of the wealthy is a bad idea, society would be better off without it.",
-            "The harder you work, the more you progress up the social ladder."
+            "Activity is not someting that can be controlled by the government.",
+            "The military is a waste of time and resources, as it has no real effect on the server.",
+            "The lottery is pointless and does nothing to increase activity",
+            "All businesses are good for activity, no matter the nature of the business.",
+            "Rules and laws generally impede activity rather than increasing it.",
+            "Economic freedom is the best way to promote economic activity."
         ],
         [//Left
-            "Organisations and corporations cannot be trusted and need regulating by the government.",
-            "A government that provides for everyone is an inherently good idea.",
-            "The current welfare system should be expanded to further combat inequality.",
-            "Land should not be a commodity to be bought and sold.",
-            "All industry and the bank should be nationalised.",
-            "Class is the primary division of society.",
-            "Economic inequality is too high in the world."
+            "The military should host more events.",
+            "The government should work with businesses that spur activity, such as FMCSOC.",
+            "Military conscription should be introduced.",
+            "The Forums should maintain and/or expand its cooperation with other nations.",
+            "National events such as Ping Yusuf Tuesday are important to maintain activity.",
+            "The fight against romanism should be emphasized more.",
+            "The government should fund businesses that are commercially viable to promote economic activity."
+            "Their should be an activity minimum for elected officials."
         ]],[//Authority-Liberty`
         [//Authority
-            "Sometimes it is right that the government may spy on its citizens to combat extremists and terrorists.",
-            "Authority figures, if morally correct, are a good thing for society.",
-            "Strength is necessary for any government to succeed.",
-            "Only the government can fairly and effectively regulate organisations.",
-            "Society requires structure and bureaucracy in order to function.",
-            "Mandatory IDs should be used to ensure public safety.",
-            "In times of crisis, safety becomes more important than civil liberties.",
-            "If you have nothing to hide, you have nothing to fear."
+            "Unelected bodies are a necessary part of any mock government.",
+            "The RIS9 plays an important role in protecting the Forums that requires it to be unelected.",
+            "I support DM Society Laws.",
+            "News networks should not be used as personal soapboxes.",
+            "Businesses should only exist if they provide legitimate economic or entertainment value.",
+            "Limits on gambling are necessary to prevent economic downturn.",
+            "The Powerful Council should have a say in proposals that modify the constitution."
         ],
         [//Liberty
-            "The government should be less involved in the day to day life of its citizens.",
-            "Without democracy, a society is nothing.",
-            "Jury nullification should be legal.",
-            "The smaller the government, the freer the people.",
-            "The government should, at most, provide emergency services and law enforcement.",
-            "The police was not made to protect the people, but to uphold the status-quo by force.",
-            "State schools are a bad idea because our state shouldn't be influencing our children."
+            "The Powerful Council should be abolished.",
+            "Taxation is unnecessary.",
+            "Executive orders give the President too much power.",
+            "Archivism has gone too far.",
+            "Freedom of speech should be absolute, unless it violates TOS.",
+            "I support the unbanning of Mice and/or Weegee.",
+            "Citizens should be allowed to campaign using DMs, within reason."
         ]],[//Progressive-Conservative
         [//Progressive
-            "Two consenting individuals should be able to do whatever they want with each other, even if it makes me uncomfortable.",
-            "An individual’s body is their own property, and they should be able to do anything they desire to it.",
-            "A person should be able to worship whomever or whatever they want.",
-            "Nudism is perfectly natural.",
-            "Animals deserve certain universal rights.",
-            "Gender is a social construct, not a natural state of affairs.",
-            "Laws based on cultural values, rather than ethical ones, aren’t justice.",
-            "Autonomy of body extends even to minors, the mentally ill, and serious criminals."
+            "I generally participate in a variety of mock governments, rather than just Caiti.",
+            "I don't care about the history of the Forums.",
+            "Policies are the primary thing I consider when voting.",
+            "Caiti should be as diplomatically active as possible.",
+            "Other nations are superior to Caiti in many ways, and should take notes from them.",
+            "Political polarization must be eliminated at all costs.",
+            "When thinking of leaders of the Foruns, I think of the President before the Powerfuls.",
+            "I do not identify with any Forumer ethnicity."
         ],
         [//Conservative
-            "Homosexuality is against my values.",
-            "Transgender individuals should not be able to adopt children.",
-            "Drugs are harmful and should be banned.",
-            "The death penalty should exist for certain crimes.",
-            "Victimless crimes should still be punished.",
-            "One cannot be moral without religion.",
-            "Parents should hold absolute power over their children, as they are older and more experienced.",
-            "Multiculturalism is bad."
+            "Romanists should not be allowed in government.",
+            "The distinction of Honoured Frumer is important to honouring our nation's history.",
+            "I consider myself Intarian/Karanoian/Southvisean moreso than I consider myself a Forumer.",
+            "I tend to vote based on who I like the most.",
+            "Caiti is superior to most neighboring mock governments in most regards.",
+            "The Forum Museum should be expanded and/or advertised more.",
+            "New members should be investigated before being verified, beyond simply the verification questions.",
+            "I consider myself a panforumist."
         ]
     ]];
